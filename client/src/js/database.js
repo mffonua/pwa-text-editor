5408234a14c6b8e14e5cb9e6e94fda1258337384
mffonua <108596346+mffonua@users.noreply.github.com>
2022-12-01 17:56:07 -0700

    updating db.js

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -12,10 +12,41 @@ const initdb = async () =>
     },
   });
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+// Get Function
+export const getDb = async (value) => {
+  console.log('Getting data from the jateDB');
 
-// TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+  const jateDb = await openDB('jate', 1);
+
+  const tx = jateDb.transaction('jate', 'readwrite');
+
+  const objStore = tx.objectStore('jate');
+
+  const req = objStore.getAll()
+
+  const res = await req;
+  console.log('data saved to the jateDB', res);
+
+};
+
+
+
+
+
+// Put Function
+export const putDb = async (id, value) => {
+  console.log('PUT request to update the jateDB');
+
+  const jateDb = await openDB('jate', 1);
+
+  const tx = jateDb.transaction('jate', 'readwrite');
+
+  const objStore = tx.objectStore('jate');
+
+  const req = objStore.put({ id: id, value: value })
+
+  const res = await req;
+  console.log('data saved to the jateDB', res);
+};
 
 initdb();
